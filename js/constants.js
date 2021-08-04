95851df614555d6580b724b2b34b2ddbbf9474de
joseliojunior <joseliojunior@users.noreply.github.com>
2021-08-04 09:35:09 -0300

    license update

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,10 @@
 /**
+ * @author Josélio de S. C. Júnior <joseliojrx25@gmail.com>
+ * @copyright Josélio de S. C. Júnior - 2021
+ * @license MIT
+ */
+
+/**
  * Identity of important features.
  */
 const constants = {
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /**
  * @author Josélio de S. C. Júnior <joseliojrx25@gmail.com>
- * @copyright Josélio de S. C. Júnior 2021
+ * @copyright Josélio de S. C. Júnior - 2021
+ * @license MIT
  */
 `use strict`
 import { Layout } from '../../../js/Classes.js';
